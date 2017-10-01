2e2a61ee0ac532874ae0c5681b94549aba3bf5f5
Angus Thomsen <aksthomsen@gmail.com>
2017-10-01 20:29:26 +1100

    simple materialized view

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+import 'babel-polyfill';
+import { Client } from 'pg';
+
 export async function handler (event, context, cb) {
-  cb();
+    const client = new Client({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        port: 5432,
+    });
+
+    try {
+        await client.connect();
+        await client.query('REFRESH MATERIALIZED VIEW todays_bookings');
+        await client.end();
+        cb(null, 'bingo');
+    }
+    catch (e) {
+        cb(e);
+    }
 }
